414dbf1a465f1862910ec12215c6a70ef1677fa0
muhammedturkyilmaz <muhammed.turkyilmaz29@gmail.com>
2021-10-24 13:05:10 +0300

    Add project files.

---
+++ Oop2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Muhammed";
+            musteri1.Soyadi = "Trkylmz";
+            musteri1.TcNo = "222222222";
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //Gerçek-Tüzel
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+        }
+    }
+}
